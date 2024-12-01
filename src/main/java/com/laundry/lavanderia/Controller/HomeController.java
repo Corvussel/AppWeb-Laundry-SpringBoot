31a1f3736817a1914c9661e35b40460a44def2f1
Russel <u00031050@sise.com.pe>
2024-12-01 15:27:48 -0500

    modificacion controllers

---
+++ src/main/java/com/laundry/lavanderia/Controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.laundry.lavanderia.Controller;
+
+import org.springframework.stereotype.Controller; 
+import org.springframework.web.bind.annotation.GetMapping; 
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String getHome() {
+
+        return "Home/index";
+    }
+
+}
