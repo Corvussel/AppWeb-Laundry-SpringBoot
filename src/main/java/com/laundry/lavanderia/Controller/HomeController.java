888388cd5250c03a9a5fb180e04e2fb9d234aece
Russel <u00031050@sise.com.pe>
2024-12-02 20:46:33 -0500

    Se trabajo el layout

--- src/main/java/com/laundry/lavanderia/Controller/HomeController.java
+++ src/main/java/com/laundry/lavanderia/Controller/HomeController.java
@@ -1,15 +1,26 @@
 package com.laundry.lavanderia.Controller;
 
-import org.springframework.stereotype.Controller; 
-import org.springframework.web.bind.annotation.GetMapping; 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
+    public String getLayout() {
+
+        return "Shared/_Layout";
+    }
+
+    @GetMapping("/home")
     public String getHome() {
 
         return "Home/index";
     }
 
+    @GetMapping("/DeliveryLaundry")
+    public String getDeliveryLaundry() {
+
+        return "DeliveryLaundry/index";
+    }
 }
