ca1be2bde18db81a8c393f58b514e5e188e5c477
ItamiDeveloper <carlosedu062010@hotmail.com>
2024-12-04 23:29:32 -0500

    Agregando Contenido Login

--- src/main/java/com/laundry/lavanderia/Controller/HomeController.java
+++ src/main/java/com/laundry/lavanderia/Controller/HomeController.java
@@ -1,9 +1,13 @@
 package com.laundry.lavanderia.Controller;
 
+ 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping; 
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import com.laundry.lavanderia.Model.Login.login; 
 
 @Controller
 @RequestMapping("/") //nombre del mapping
@@ -34,5 +38,25 @@ public class HomeController {
  
         model.addAttribute("content", "services-laundry/index.html");
         return "shared/layout";
+    }
+    // Estoy creando la nueva redireccion para Login - Caballero
+    @GetMapping("/login")
+    public String getLoginPage(Model model) {
+        model.addAttribute("login", new login()); // Pasamos un objeto vacío de Login
+        model.addAttribute("content", "login/index.html");
+        return "login";
+    }
+    // Con autenticacion - Caballero
+    @PostMapping("/authenticate")
+    public String authenticate(@ModelAttribute("login") login login, Model model) {
+        // Simulación de validación
+        if ("admin".equals(login.getUsername()) && "123".equals(login.getPassword())) {
+            model.addAttribute("message", "Inicio de sesión exitoso");
+            model.addAttribute("content", "home/index.html");
+        } else {
+            model.addAttribute("message", "Usuario o contraseña incorrectos");
+            model.addAttribute("content", "login/index.html");
+        }
+        return "authenticate";
     }
 }
