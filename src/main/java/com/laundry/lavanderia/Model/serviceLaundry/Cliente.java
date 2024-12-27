d6de73722f1bb4af20d9f3fa87f9054223ee0961
Sahel-pl <u00030737@sise.com.pe>
2024-12-27 05:54:40 -0500

    CRUD CLIENTES (javascript me ayudo)

--- src/main/java/com/laundry/lavanderia/Model/serviceLaundry/Cliente.java
+++ src/main/java/com/laundry/lavanderia/Model/serviceLaundry/Cliente.java
@@ -2,6 +2,8 @@ package com.laundry.lavanderia.Model.serviceLaundry;
 
 import java.util.List;
 
+import lombok.Data;
+@Data
 public class Cliente {
 
     private Long id;
@@ -13,8 +15,9 @@ public class Cliente {
     private String direccion;
     private String distrito;
     private List <Cliente> clientes;
+    private boolean activo;
 
-    public Cliente(List<Cliente> clientes, String codigoCliente, String direccion, String distrito, String genero, Long id, String nombre, String observaciones, String telefono) {
+    public Cliente(List<Cliente> clientes, String codigoCliente, String direccion, String distrito, String genero, Long id, String nombre, String observaciones, String telefono, boolean activo) {
         this.clientes = clientes;
         this.codigoCliente = codigoCliente;
         this.direccion = direccion;
@@ -24,83 +27,8 @@ public class Cliente {
         this.nombre = nombre;
         this.observaciones = observaciones;
         this.telefono = telefono;
+        this.activo= activo;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getTelefono() {
-        return telefono;
-    }
-
-    public void setTelefono(String telefono) {
-        this.telefono = telefono;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public String getObservaciones() {
-        return observaciones;
-    }
-
-    public void setObservaciones(String observaciones) {
-        this.observaciones = observaciones;
-    }
-
-    public String getCodigoCliente() {
-        return codigoCliente;
-    }
-
-    public void setCodigoCliente(String codigoCliente) {
-        this.codigoCliente = codigoCliente;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public String getDistrito() {
-        return distrito;
-    }
-
-    public void setDistrito(String distrito) {
-        this.distrito = distrito;
-    }
-
-    public List<Cliente> getClientes() {
-        return clientes;
-    }
-
-    public void setClientes(List<Cliente> clientes) {
-        this.clientes = clientes;
-    }
-
-
-
-
 
 
 }
