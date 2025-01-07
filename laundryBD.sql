dd994a1589175cbb0b5b819ac7685e4b1bb5d0b4
Flores <u00031050@sise.com.pe>
2025-01-07 13:35:22 -0500

    include bd

--- laundryBD.sql
+++ laundryBD.sql
@@ -1,73 +1,155 @@
+-- Active: 1735826042611@@b0vg062se0kqukm3t4yx-mysql.services.clever-cloud.com@3306@b0vg062se0kqukm3t4yx
 CREATE DATABASE sistema_ventas;
 
 USE sistema_ventas;
 
-CREATE TABLE Clientes (
-    ID_Cliente INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(50),
-    Apellido VARCHAR(50),
-    Correo_Electronico VARCHAR(100),
-    Telefono VARCHAR(20),
-    Direccion VARCHAR(255)
+CREATE TABLE persistent_logins (
+    username VARCHAR(64) NOT NULL,
+    series VARCHAR(64) PRIMARY KEY,
+    token VARCHAR(64) NOT NULL,
+    last_used TIMESTAMP NOT NULL
 );
 
-CREATE TABLE Empleados (
-    ID_Empleado INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(50),
-    Apellido VARCHAR(50),
-    Cargo VARCHAR(50),
-    Correo_Electronico VARCHAR(100),
-    Telefono VARCHAR(20)
+CREATE TABLE role (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE Ventas (
-    ID_Venta INT AUTO_INCREMENT PRIMARY KEY,
-    Fecha DATETIME,
-    Total DECIMAL(10, 2),
-    ID_Cliente INT,
-    ID_Empleado INT,
-    FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente),
-    FOREIGN KEY (ID_Empleado) REFERENCES Empleados(ID_Empleado)
+CREATE TABLE employee (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    phone VARCHAR(255) NOT NULL,
+    role_id BIGINT,
+    status BOOLEAN DEFAULT true,
+    password VARCHAR(255) NOT NULL,
+    profile_image VARCHAR(255),
+    FOREIGN KEY (role_id) REFERENCES role(id)
 );
 
-CREATE TABLE Productos_Servicios (
-    ID_Producto_Servicio INT AUTO_INCREMENT PRIMARY KEY,
-    Nombre VARCHAR(100),
-    Descripcion TEXT,
-    Categoria VARCHAR(50),
-    Precio DECIMAL(10, 2)
+CREATE TABLE cliente (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    telefono VARCHAR(255) NOT NULL,
+    direccion VARCHAR(255) NOT NULL,
+    activo BOOLEAN NOT NULL,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Detalles_Venta (
-    ID_Detalle_Venta INT AUTO_INCREMENT PRIMARY KEY,
-    ID_Venta INT,
-    ID_Producto_Servicio INT,
-    Cantidad INT,
-    Precio DECIMAL(10, 2),
-    FOREIGN KEY (ID_Venta) REFERENCES Ventas(ID_Venta),
-    FOREIGN KEY (ID_Producto_Servicio) REFERENCES Productos_Servicios(ID_Producto_Servicio)
+CREATE TABLE category (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    active BOOLEAN NOT NULL
+);
+
+CREATE TABLE service_laundry (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    price DOUBLE NOT NULL,
+    description VARCHAR(255),
+    active BOOLEAN NOT NULL,
+    duration INT NOT NULL,
+    category_id BIGINT,
+    FOREIGN KEY (category_id) REFERENCES category(id)
+);
+
+CREATE TABLE boleta (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    numero_boleta VARCHAR(255) NOT NULL,
+    fecha_generada TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE payment_method (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR(255),
+    active BOOLEAN DEFAULT true
+);
+
+CREATE TABLE cash_closing (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    closing_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    total_income DOUBLE NOT NULL,
+    total_expenses DOUBLE NOT NULL,
+    final_balance DOUBLE NOT NULL,
+    observations VARCHAR(255),
+    employee_id BIGINT NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
+);
+
+CREATE TABLE order_service (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    employee_id BIGINT,
+    client_id BIGINT,
+    boleta_id BIGINT,
+    total_servicio DOUBLE NOT NULL,
+    descuento DOUBLE,
+    precio_total DOUBLE NOT NULL,
+    payment_method_id BIGINT,
+    fecha TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    observacion VARCHAR(255),
+    tipo_servicio VARCHAR(255), 
+    status VARCHAR(255),
+    total_Cobro DOUBLE NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employee(id),
+    FOREIGN KEY (client_id) REFERENCES cliente(id),
+    FOREIGN KEY (boleta_id) REFERENCES boleta(id),
+    FOREIGN KEY (payment_method_id) REFERENCES payment_method(id)
 );
  
-
-CREATE TABLE Gastos (
-    ID_Gasto INT AUTO_INCREMENT PRIMARY KEY,
-    Fecha DATETIME,
-    Descripcion TEXT,
-    Monto DECIMAL(10, 2)
+CREATE TABLE order_details (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    order_service_id BIGINT,
+    nombre VARCHAR(255) NOT NULL,
+    precio_unidad DOUBLE NOT NULL,
+    cantidad INT NOT NULL,
+    sub_total DOUBLE NOT NULL,
+    detalle VARCHAR(255),
+    FOREIGN KEY (order_service_id) REFERENCES order_service(id)
 );
 
-CREATE TABLE Pedidos (
-    ID_Pedido INT AUTO_INCREMENT PRIMARY KEY,
-    ID_Cliente INT,
-    Fecha_Pedido DATETIME,
-    Estado VARCHAR(50),
-    FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente)
+CREATE TABLE spendings (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    date DATE NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    category VARCHAR(50) NOT NULL,
+    employee_id BIGINT NOT NULL,
+    amount DOUBLE NOT NULL,
+    FOREIGN KEY (employee_id) REFERENCES employee(id)
 );
+ 
+ -- ELIMINAR TABLAS
+DROP TABLE employee;
+DROP TABLE role;
+DROP TABLE cliente;
+DROP TABLE category;
+DROP TABLE boleta;
+DROP TABLE order_service;
+DROP TABLE order_details; 
+DROP TABLE service_laundry;
+DROP TABLE payment_method; 
 
-CREATE TABLE WhatsApp_Comunicaciones (
-    ID_Comunicacion INT AUTO_INCREMENT PRIMARY KEY,
-    ID_Cliente INT,
-    Fecha_Comunicacion DATETIME,
-    Mensaje TEXT,
-    FOREIGN KEY (ID_Cliente) REFERENCES Clientes(ID_Cliente)
-);
+-- SELECCIONAR TABLAS
+SELECT * FROM persistent_logins;
+SELECT * FROM employee;
+SELECT * FROM role;
+SELECT * FROM cliente;
+SELECT * FROM category;
+SELECT * FROM service_laundry;
+SELECT * FROM payment_method;
+SELECT * FROM boleta;
+SELECT * FROM order_service;
+SELECT * FROM order_details;
+SELECT * FROM spendings;
+SELECT * FROM cash_closing;
+
+INSERT INTO role (name) VALUES ('admin');
+INSERT INTO role (name) VALUES ('employee');
+
+
+
+
+
+
