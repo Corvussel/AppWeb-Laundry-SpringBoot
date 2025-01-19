424b9bb17112684be90f5dde22f1275bfd24cc88
RUSSEL <russelfloressolano900@gmail.com>
2025-01-18 20:46:21 -0500

    documentacion servicio boleta impl

--- src/main/java/com/laundry/lavanderia/service/BoletaServiceImpl.java
+++ src/main/java/com/laundry/lavanderia/service/BoletaServiceImpl.java
@@ -1,6 +1,5 @@
 package com.laundry.lavanderia.service;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.laundry.lavanderia.Model.serviceLaundry.Boleta;
@@ -13,13 +12,23 @@ public class BoletaServiceImpl implements IBoletaService {
     @Autowired
     private BoletaRepository boletaRepository;
 
+    /**
+     * guardar boleta en la base de datos 
+     *
+     * @param boleta la boleta que se va a guardar
+     * @return la boleta guardada en la base de datos.
+     */ 
     public Boleta saveBoleta(Boleta boleta) {
         return boletaRepository.save(boleta);
     }
 
+    /**
+     * buscar boleta por numero de boleta
+     *
+     * @param numeroBoleta el numero de boleta
+     * @return la boleta encontrada
+     */
     public Boleta findByNumeroBoleta(String numeroBoleta) {
         return boletaRepository.findByNumeroBoleta(numeroBoleta);
     }
 }
-
- 
